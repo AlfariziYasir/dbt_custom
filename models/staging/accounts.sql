9f1b11db6a810a74cd3048ae1c71d187f38441c9
AlfariziYasir <yasiralfarizi@gmail.com>
2024-02-26 11:56:02 +0700

    test

--- models/staging/accounts.sql
+++ models/staging/accounts.sql
@@ -1,6 +1,3 @@
-create extension dblink;
-
-SELECT dblink_connect('myihx','host=localhost port=5432 dbname=myihx user=postgres password=postgres options=-csearch_path=');
-
-select * from dblink('myihx', 'select * from emis.accounts')
-as t
+select
+   *
+from {{ source('emis', 'accounts') }}
